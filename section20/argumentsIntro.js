549634f70e38877ad39aa3f0d7f3f8e23d2231a1
Nikhil Math <erulemath@gmail.com>
2021-01-18 17:54:34 -0700

    did a few more stuff

---
+++ section20/argumentsIntro.js
@@ -0,0 +1,11 @@
+function greet(firstName, lastName) {
+  console.log(`hi ${firstName} ${lastName}!`);
+  console.log(`or should I say ${firstName} ${lastName[0]}.`)
+}
+greet("george", "clooney")
+
+function repeat(str, numTime) {
+  for (let i = 0; i = numTime; i++){
+    console.log("HI")
+  }
+}
---
+++ section20/ourVeryFirstFunction.js
@@ -0,0 +1,16 @@
+function singSong() {
+  console.log("DO")
+  console.log("RE")
+  console.log("MI")
+}
+
+singSong()
+
+
+
+
+// function grumpus() {
+//   console.log('ugh you again')
+//   console.log('for the last time')
+//   console.log('leave me alone')
+// }
